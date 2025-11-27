d3a5e3446b4d231d4fa17b0a25d492c7aea59884
evflorezm <evflorezm@unal.edu.co>
2025-11-26 23:31:26 -0500

    Solución PRE-24

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,43 +1,10 @@
-"""Autograding script."""
+"""Autograding script"""
 
-
-def load_data():
-
-    import pandas as pd
-
-    dataset = pd.read_csv("files/heart_disease.csv")
-    y = dataset.pop("target")
-    x = dataset.copy()
-    x["thal"] = x["thal"].map(
-        lambda x: "normal" if x not in ["fixed", "fixed", "reversible"] else x
-    )
-
-    return x, y
-
-
-def load_estimator():
-
-    import os
-    import pickle
-
-    if not os.path.exists("homework/estimator.pickle"):
-        return None
-    with open("homework/estimator.pickle", "rb") as file:
-        estimator = pickle.load(file)
-
-    return estimator
+import os
 
 
 def test_01():
-
-    from sklearn.metrics import accuracy_score
-
-    x, y = load_data()
-    estimator = load_estimator()
-
-    accuracy = accuracy_score(
-        y_true=y,
-        y_pred=estimator.predict(x),
-    )
-
-    assert accuracy > 0.8413
+    """Check if the files are present"""
+    assert os.path.exists("homework/digits_pca.png")
+    assert os.path.exists("homework/digits_tsne.png")
+    assert os.path.exists("homework/digits_umap.png")
